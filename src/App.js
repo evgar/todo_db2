ff9d6617681708895c611f3900cf8c3ffe661e22
evgar <eugene.garichev@gmail.com>
2018-06-30 21:55:11 +0300

    add sorting functionality

--- src/App.js
+++ src/App.js
@@ -8,19 +8,8 @@ class App extends Component {
 		super();
 
 		this.state = {
-			todoItems: [
-				{
-					number: 1,
-					name: 'One'
-				},
-				{
-					number: 2,
-					name: 'Two'
-				}, {
-					number: 3,
-					name: 'Three'
-				},
-			]
+			todoItems: [],
+			sortedBy: 'number'
 		}
 	}
 
@@ -28,20 +17,26 @@ class App extends Component {
 		return (
 			<section>
 				<h3>TODO list</h3>
-				<ListInput onListInputSubmit={e => this.getNewItem(e)}/>
-				<Toggle/>
-				<ItemsList items={this.state.todoItems}/>
+				<ListInput onListInputSubmit = {e => this.getNewItem(e)}/>
+				<Toggle onSortingChange = {e => this.changeSorting(e)}/>
+				<ItemsList items={this.state.todoItems} sortedType = {this.state.sortedBy}/>
 			</section>
 		);
 	}
 
 	getNewItem(e) {
 		let item = {
-			name: e
+			name: e,
+			number: this.state.todoItems.length + 1
 		};
 		this.setState({
-			todoItems: [...this.state.todoItems,  item]
+			todoItems: [item, ...this.state.todoItems]
 		});
+	}
+
+	changeSorting(e) {
+		let sortedType = e.target.checked ? 'number' : 'name';
+		this.setState({sortedBy: sortedType});
 	}
 }
 
@@ -70,6 +65,7 @@ class ListInput extends Component {
 	handleSubmit(event) {
 		event.preventDefault();
 		this.props.onListInputSubmit(this.state.value);
+		this.setState({value: event.target.value = ''});
 	}
 }
 
@@ -79,25 +75,45 @@ class Toggle extends Component {
 			<div>
 				<p>Sort by:</p>
 				<span>Name</span>
-				<input type="checkbox"/>
+				<input onChange={e => this.handleChange(e)} type="checkbox"/>
 				<span>Number</span>
 			</div>
 		)
+	}
+
+	handleChange(event) {
+		this.props.onSortingChange(event);
 	}
 }
 
 class ItemsList extends Component {
 	render() {
-
 		return (
 			<ol>
 				{
-					this.props.items.map((item, i) => (
-						<li key={i}> {item.name}</li>
+					this.props.items.sort((item1, item2) => {
+						if (this.props.sortedType === 'number') {
+							return item1.number - item2.number;
+						} else if (this.props.sortedType === 'name') {
+							this.props.items.sort((item1, item2) => {
+								if (item1.name < item2.name) return -1;
+								if (item1.name > item2.name) return 1;
+								return 0;
+							})
+						}
+					}).map((item, index) => (
+						<li key={index}>{item.number} {item.name}
+							<button onClick={() => this.clickHandler(index)}>Remove</button>
+						</li>
 					))
 				}
 			</ol>
 		)
+
+
+	}
+	clickHandler(index) {
+		console.log(index);
 	}
 }
 
